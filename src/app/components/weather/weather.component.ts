64e1e8b5e9d39e337c6f905fad234f78a6e752b8
cdekk3r <cdekk3r@hotmail.com>
2018-08-15 16:57:21 -0400

    fixed weather component view

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-weather',
-    template: './weather.component.html'
+    templateUrl: './weather.component.html'
 })
 export class WeatherComponent {
 
